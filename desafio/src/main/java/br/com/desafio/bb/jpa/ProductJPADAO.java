ec9f7e5f1db62785012702deff55b58e45d5d8f6
Alexandre Ferreira <alexandre.ferreira@gesplan.com.br>
2017-03-14 13:22:32 -0300

    Adicionado consulta de produtos

--- desafio/src/main/java/br/com/desafio/bb/jpa/ProductJPADAO.java
+++ desafio/src/main/java/br/com/desafio/bb/jpa/ProductJPADAO.java
@@ -9,7 +9,23 @@ public class ProductJPADAO  extends JPAAbstract<Product,Integer> implements Prod
 	
 	public List<Product> getListProduct() {
 		
-		return this.list("SELECT E FROM " + this.getEntityName() + " E");
+		return this.list("SELECT t1.id,t1.description,t1.note,t1.owner_id E FROM " + this.getEntityName() + " E  AS t1 LEFT JOIN product t2 ON t1.id = t2.owner_id WHERE t2.owner_id IS NULL");
 	}
 }
 
+//SELECT t1.id,t1.description,t1.note,t1.owner_id FROM
+//product AS t1 LEFT JOIN product t2
+//ON t1.id = t2.owner_id
+//WHERE t2.owner_id IS NULL;
+
+//SELECT t1.id,t1.description,t1.note,t1.owner_id FROM
+//product AS t1 LEFT JOIN product t2
+//ON t1.id = t2.owner_id
+//WHERE t2.owner_id IS NULL;
+//
+//SELECT t1.description AS lev1, t2.description as lev2, t3.description as lev3, t4.description as lev4
+//FROM product AS t1
+//LEFT JOIN product AS t2 ON t2.owner_id = t1.id
+//LEFT JOIN product AS t3 ON t3.owner_id = t2.id
+//LEFT JOIN product AS t4 ON t4.owner_id = t3.id
+//WHERE t1.description = 'masculino';
